9a0507e3659b4c64eb031f1a1595a9de0c2aa6fa
zoosewu <>
2021-01-15 23:29:27 +0800

    新增改版資訊 說明葉面排版更新

--- src/app/connect/PluginSettings/ConnectNewVersionInfo.js
+++ src/app/connect/PluginSettings/ConnectNewVersionInfo.js
@@ -5,7 +5,6 @@ export let ConnectNewVersionInfo = {
       if (this.pluginWidth < 399) return "px-0";
       else return "px-5";
     },
-
   },
   template: `<div :class="Classes">
   <h4 class="text-center my-1">近期改版</h4>
@@ -13,6 +12,8 @@ export let ConnectNewVersionInfo = {
   <hr class="mt-1 mb-2">
   <p class="mt-1 mb-0">支援Twitch</p>
   <p class="mt-1 mb-0">推文內的連結可以直接點開，圖片可以直接預覽</p>
+  <p class="mt-1 mb-0">移除前三行的文章可以正常讀取。</p>
+  <p class="mt-1 mb-0">例如:#1W00ZXmh (LoL)</p>
   <hr class="mt-1 mb-2">
   <p class="mt-1 mb-0">新增套件長度、推文更新、字體尺寸、推文間隔</p>
   <p class="mt-1 mb-0">套件長度最大值現在可以到850</p>
