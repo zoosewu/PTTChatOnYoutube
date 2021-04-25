f28568bc0e89cb4c801f4ce3e323f1fd58921206
zoosewu <>
2021-04-25 16:58:49 +0800

    支援holodex

---
+++ src/SupportWebsite/holodex/InitHD.js
@@ -0,0 +1,52 @@
+
+import { InitApp } from '../../app/appindex.js';
+import { BootStrap } from '../../BootStrap.js';
+import { AddStyle, GenerateCryptKey, paddingLeft, paddingRight, dateReviver, clone, ThemeCheck } from '../../library.js';
+
+export function InitHD(messageposter) {
+  //Check Theme
+  let WhiteTheme = ThemeCheck('html', '250, 250, 250');
+
+  //run app instance loop
+  let waswatch;
+  let iswatch;
+  let tryinsholotools = 20;
+
+  (function ChechChatInstanced() {
+    setTimeout(ChechChatInstanced, 1000);
+    const watchcheck = /https:\/\/holodex\.net\/multiview/.exec(window.location.href);
+    if (watchcheck) iswatch = watchcheck[0];
+    else iswatch = false;
+    if (waswatch !== iswatch && iswatch) {
+      tryinsholotools = 20;
+    }
+    if (tryinsholotools >= 0) {
+      TryInsChat();
+    }
+    waswatch = iswatch;
+  })();
+  function TryInsChat() {
+    // const parent = $(`.v-main__wrap`);
+    const parent = $(`.vue-grid-layout`).parent();
+    if (reportmode) console.log("parent", parent);
+    if (parent.length > 0 && iswatch) {
+      const pluginwidth = GM_getValue('PluginWidth', 400);
+      const fakeparent = $(`<div id="fakeparent" class="d-flex flex-row"></div>`);
+      const defaultVideoHandler = $(`<div id="holotoolsvideohandler" style="flex:1 1 auto;"></div>`);
+      // const defaultVideo = $(`.vue-grid-layout`).parent();
+      const defaultVideo = $(`.vue-grid-layout`);
+
+      const PTTChatHandler = $(`<div id="pttchatparent" class="p-0 d-flex" style="flex:0 0 ` + pluginwidth + `px;position:relative;"></div>`);
+      parent.append(fakeparent);
+      fakeparent.append(defaultVideoHandler);
+      defaultVideoHandler.append(defaultVideo);
+      fakeparent.append(PTTChatHandler);
+      $(`.reopen-toolbar`).css({ "z-index": "302" });
+      InitApp(PTTChatHandler, WhiteTheme, true, messageposter, true);
+      tryinsholotools = -10;
+    }
+    else {
+      tryinsholotools--;
+    }
+  }
+}
---
+++ src/SupportWebsite/holodex/hdfilter.js
@@ -0,0 +1,4 @@
+import { InsFilter } from '../../filter/InsFilter.js';
+import { InitHD } from './InitHD.js';
+
+export let hdfilter = InsFilter("Holodex", /holodex\.net/, "https://holodex.net", InitHD); 
