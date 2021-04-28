16f214ae7356a2c3874803c9d480634bb277363c
crimsonmoon9 <crimsonmoon9@users.noreply.github.com>
2021-04-28 23:12:28 +0800

    修改成新增選項後會關閉輸入框及新增至選單頂端，點擊選單選項後將其移至頂端方便下次使用，移除預覽確認按紐，修改下拉選單結構，修復搜尋同一文章時會找錯文章的問題。

--- src/app/connect/ConnectConnectSetting/ConnectAutoFetchPostDropDownElement.js
+++ src/app/connect/ConnectConnectSetting/ConnectAutoFetchPostDropDownElement.js
@@ -10,11 +10,16 @@ export let ConnectAutoFetchPostDropDownElement = {
       dropdownPreview: null,
       board: null,
       title: null,
-      enableManualSetting: false,
       connectAutoFetchPost_manualBoard: null,
       connectAutoFetchPost_manualTitle: null,
-      showPreviewForm: false,
       SetingValue_previewTitle: null,
+    }
+  },
+  watch: {
+    SetingValue_previewTitle: function () {
+      $("#previewForm").collapse("show");
+      $("#manualinputarea").collapse("hide");
+      this.getPost();
     }
   },
   mounted() {
@@ -24,50 +29,33 @@ export let ConnectAutoFetchPostDropDownElement = {
     };
   },
   methods: {
-    $_ConnectAutoFetchPost_onClickRemoveOption(_parent) {
-      const c = this.optionGroup;
-      this.optionGroup = {};
-      this.optionGroup = c;
-      delete this.optionGroup[_parent];
+    $_ConnectAutoFetchPost_onClickRemoveOption(_index) {
+      this.optionGroup.splice(_index, 1);
       this.$store.dispatch("setSearchTitle", this.optionGroup);
     },
-    $_ConnectAutoFetchPost_onClickDropdownItem(_board, _species) {
-      this.board = _board; 
-      this.title = this.optionGroup[_board][_species];
-      this.dropdownPreview = _board + " " + _species;
-      this.showPreviewForm = true;
-    },
-    $_ConnectAutoFetchPost_onClickDropdownItem(_parents) {
-      const result = /(.+) \((.+)\)/.exec(_parents);
+    $_ConnectAutoFetchPost_onClickDropdownItem(_item, _index) {
+      $("#manualinputarea").collapse("hide");
+      const result = /(.+) \((.+)\)/.exec(_item);
       this.board = result[2];
       this.title = result[1];
       this.dropdownPreview = result[1] + " (" + result[2] + ")";
-      this.showPreviewForm = true;
-      const self = this;
-      $(this.$refs.preview).on('shown.bs.collapse', ()=>{ 
-        $(self.$refs.preview).removeAttr('id');
-      });    
+      this.optionGroup.splice(0, 0, this.optionGroup.splice(_index, 1)[0]);
+      this.$store.dispatch("setSearchTitle", this.optionGroup);
+      this.msg.PostMessage("getPostTitle", { boardforsearch: this.board, titleforsearch: this.title });
     },
-    $_ConnectAutoFetchPost_enableManualSetting() {
-      //this.showPreviewForm = true;
-      const self = this;
-      $(this.$refs.manualinput).on('shown.bs.collapse', ()=>{ 
-        if (!this.showPreviewForm) $(self.$refs.preview).collapse('toggle');
-      });
-      this.enableManualSetting = true;
-    },
-    getPostTitle: function () {
+    addAndSearch: function () {
       if ( this.connectAutoFetchPost_manualBoard !== null && this.connectAutoFetchPost_manualTitle !== null) {
-        this.board = this.connectAutoFetchPost_manualBoard; 
-        this.title = this.connectAutoFetchPost_manualTitle;
-        this.optionGroup[this.title + " (" + this.board + ")"] = this.title + " (" + this.board + ")";
-        this.$store.dispatch("setSearchTitle", this.optionGroup);
+        this.optionGroup.unshift(this.connectAutoFetchPost_manualTitle + " (" + this.connectAutoFetchPost_manualBoard + ")");
+      }
+      else {
+        this.$store.dispatch('Alert', { type: 0, msg: "看板名稱及標題不得為空。" });
+        return;
       }
       if (this.PTTState < 1) {
         this.$store.dispatch('Alert', { type: 0, msg: "PTT尚未登入，請先登入。" });
         return;
       }
-      this.msg.PostMessage("getPostTitle", { boardforsearch: this.board, titleforsearch: this.title });
+      this.$_ConnectAutoFetchPost_onClickDropdownItem(this.optionGroup[0]);
     },
     getPost: function () {
       //if (reportmode) console.log("click AutoFetchPostBtn" + this.board + " " + this.title + " " + this.SetingValue_previewTitle);
@@ -80,70 +68,53 @@ export let ConnectAutoFetchPostDropDownElement = {
     },
     checkOptionGroup: function() {
       let option = this.$store.getters["getSearchTitle"];
-      if (option === null || (option !== null && Object.keys(option).length === 0)) 
-      return {
-        "直播單 (C_Chat)": "直播單 (C_Chat)",
-        "彩虹直播 (Vtuber)": "彩虹直播 (Vtuber)"
-      }
+      if (option === null || option.length === 0)
+      return [
+        "直播單 (C_Chat)",
+        "彩虹直播 (Vtuber)"]
       else return option;
     },
   },
   computed: {
     DisplayOption() { return  this.dropdownPreview === null ? "請選擇...." : this.dropdownPreview},
-    TwoLevelDict: function () {
-      let container = {};
-      for ( [parents, children] of Object.entries(this.optionGroup)) {
-        if(typeof children === 'object' && Object.keys(children).length !== 0) container[parents] = this.optionGroup[parents];
-      }
-      return container;
-    },
-    OneLevelDict: function () { 
-      let container = {};
-      for ( [parents, children] of Object.entries(this.optionGroup)) {
-        if(typeof children !== 'object' || Object.keys(children).length === 0) container[parents] = this.optionGroup[parents];
-      }
-      return container;
-    },
     ...Vuex.mapGetters(['PTTState'])
   },
-  template: `<div class="form-group">
+  template: `<div class="form-group my-3">
   <div class="form-row mt-3 mb-2">
     <label class="col-3 col-form-label">{{this.description}}</label>
     <div class="col">
-      <div class="dropdown" v-show="!enableManualSetting">
-        <button class="btn ptt-btnoutline dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+      <div class="dropdown">
+        <button class="btn ptt-btnoutline dropdown-toggle" type="button" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">
           {{this.DisplayOption}}
         </button>
-        <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-          <li class="dropdown-submenu" v-for="(item, parents) in TwoLevelDict" v-show="Object.keys(TwoLevelDict).length !== 0">
-            <a class="dropdown-item" tabindex="-1" href="#" @click.prevent>{{parents}}</a>
-            <ul class="dropdown-menu">
-              <li class="dropdown-item" type="button" v-for="(item, children) in item" data-toggle="collapse" data-target="#previewtitle"><a tabindex="-1" href="#"
-                  @click.prevent="$_ConnectAutoFetchPost_onClickDropdownItem(parents, children)">{{children}}</a></li>
-            </ul>
-          </li>
-          <a href="#" class="dropdown-item" v-for="(item, parents) in OneLevelDict" data-toggle="collapse" data-target="#previewtitle" v-show="Object.keys(TwoLevelDict).length === 0" @click.prevent="$_ConnectAutoFetchPost_onClickDropdownItem(parents)">{{parents}}<button type="button" @click.stop="$_ConnectAutoFetchPost_onClickRemoveOption(parents)" @click.prevent class="close">&times;</button></a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+          <a href="#" class="dropdown-item" v-for="(item, index) in optionGroup"
+            @click.prevent="$_ConnectAutoFetchPost_onClickDropdownItem(item, index)">{{item}}<button type="button"
+              @click.stop="$_ConnectAutoFetchPost_onClickRemoveOption(index)" @click.prevent
+              class="close">&times;</button></a>
           <li class="dropdown-divider"></li>
-          <a href="#" class="dropdown-item" data-toggle="collapse" data-target="#manualinputarea" @click.prevent="$_ConnectAutoFetchPost_enableManualSetting()">其他</a>
+          <a href="#" class="dropdown-item" data-toggle="collapse" data-target="#manualinputarea" @click.prevent>新增其他選項</a>
         </ul>
       </div>
-      <div ref="manualinput" class="col px-0 collapse" v-on:keyup.13="getPostTitle" id="manualinputarea">
-        <input type="text" class="form-control mb-1" v-model="connectAutoFetchPost_manualBoard" placeholder="看板：">
-        <input type="text" class="form-control mt-1" v-model="connectAutoFetchPost_manualTitle" placeholder="標題：">
-      </div>
-    </div>
-    <div class="col-2 px-0">
-      <button class="btn ptt-btnoutline w-100 px-2" type="button" @click.self="getPostTitle()">搜尋</button>
     </div>
   </div>
-  <div ref="preview" class="form-row mt-2 mb-3 collapse" id="previewtitle">
+  <div class="form-row collapse" id="manualinputarea">
     <div class="col-3"></div>
-    <div class="col mx-2" style="border:1px solid;">
-      <div class="my-2">{{SetingValue_previewTitle}}</div>
+    <div class="col" v-on:keyup.13="addAndSearch">
+      <input type="text" class="form-control mb-1" v-model="connectAutoFetchPost_manualBoard" placeholder="看板：">
+      <input type="text" class="form-control mt-1" v-model="connectAutoFetchPost_manualTitle" placeholder="標題：">
     </div>
     <div class="col-2 px-0">
-      <button id="asd" class="btn ptt-btnoutline w-100 px-2" type="button" @click.self="getPost()">讀取</button>
+      <button class="btn ptt-btnoutline w-100 px-2" type="button" @click.self="addAndSearch()">新增</button>
+    </div>
+  </div>
+  <div class="form-row my-3 collapse" id="previewForm">
+    <div class="col-3">
+      <label class="col-form-label">標題預覽</label>
+    </div>
+    <div class="col ml-2" style="border:1px solid;">
+      <div class="my-2">{{SetingValue_previewTitle}}</div>
     </div>
   </div>
 </div>`,
