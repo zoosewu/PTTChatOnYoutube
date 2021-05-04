e35ccc97e633dd82fb59a4c0fb71ad9c8e0e86e6
crimsonmoon9 <crimsonmoon9@users.noreply.github.com>
2021-05-04 20:19:29 +0800

    修復新的開啟方式在舊版holotools中的錯誤，暫時移除新版holotools中的此功能。

--- src/SupportWebsite/holotools/InitHT.js
+++ src/SupportWebsite/holotools/InitHT.js
@@ -30,22 +30,36 @@ export function InitHT(messageposter) {
     const theme = $('html:eq(0)').hasClass("md-theme-hololight") ? "hololight" : "holodark";
     if (reportmode) console.log("parent", parent);
     if (parent.length > 0 && iswatch) {
-      const iconParent = $('.live-control-button:eq(0)');
-      const icon = $('<i data-v-2989253e="" class="md-icon md-icon-font md-theme-' + theme + '" data-toggle="collapse" data-target="#PTTMain">local_parking</i>');
-      iconParent.append(icon);
       const pluginwidth = GM_getValue('PluginWidth', 400);
       const pluginwidth0 = "0";
-      let now = pluginwidth0;
-      iconParent.on('click', function () {
-        now = (now === pluginwidth0 ? pluginwidth : pluginwidth0);
-        $('#pttchatparent').css("flex", "0 0 " + now + "px");
-        const defaultTypesettingBtn = $(`.md-icon.md-icon-font:eq(${$('.md-icon.md-icon-font').length - 6})`);
-        defaultTypesettingBtn.trigger("click");
-      })
+      if (/https:\/\/hololive\.jetri\.co\/#\/watch/.exec(iswatch)) {
+        const liveControls = $('.live-controls');
+        liveControls.css("width", "auto");
+        const datahash = Object.keys(liveControls.data())[0];
+        const iconParent = $(`<div data-${datahash} class="live-control live-control-quad bg-300" type="button"></div>`);
+        const icon = $(`<i data-${datahash} class="md-icon md-icon-font md-theme-${theme}" title="PTT">local_parking</i>`);
+        iconParent.append(icon);
+        liveControls.prepend(iconParent);
+        let now = pluginwidth0;
+        let collapseStart = false;
+        let collapseEnd = true;
+        iconParent.on('click', function () {
+          if (collapseEnd || !collapseStart) {
+            if (now === "0") $("#PTTMain").collapse("show");
+            else $("#PTTMain").collapse("hide");
+            now = (now === pluginwidth0 ? pluginwidth : pluginwidth0);
+            $('#pttchatparent').css("flex", "0 0 " + now + "px");
+            const defaultTypesettingBtn = $(`.md-icon.md-icon-font:eq(${$('.md-icon.md-icon-font').length - 6})`);
+            defaultTypesettingBtn.trigger("click");
+          }
+        });
+        $(document).on("show.bs.collapse hide.bs.collapse", "#PTTMain", function () { collapseStart = true; collapseEnd = false; });
+        $(document).on("shown.bs.collapse hidden.bs.collapse", "#PTTMain", function () { collapseStart = false; collapseEnd = true; });
+      }
       const fakeparent = $(`<div id="fakeparent" class="d-flex flex-row"></div>`);
       const defaultVideoHandler = $(`<div id="holotoolsvideohandler" style="flex:1 1 auto;"></div>`);
       const defaultVideo = $(`.player-container.hasControls`);
-      const PTTChatHandler = $(`<div id="pttchatparent" class="p-0 d-flex" style="flex:0 0 0px;position:relative;"></div>`);
+      const PTTChatHandler = $(`<div id="pttchatparent" class="p-0 d-flex" style="flex:0 0 ` + pluginwidth + `px;position:relative;"></div>`);
       parent.append(fakeparent);
       fakeparent.append(defaultVideoHandler);
       defaultVideoHandler.append(defaultVideo);
