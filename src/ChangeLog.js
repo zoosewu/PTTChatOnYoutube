c6a487110b0b474bf4342d74a12e385bdfaafd70
crimsonmoon9 <crimsonmoon9@users.noreply.github.com>
2021-05-10 06:13:47 +0800

    現在只要修改msg就能增刪更新日誌了。

--- src/ChangeLog.js
+++ src/ChangeLog.js
@@ -1,23 +1,34 @@
 export function ChangeLog () {
-  if (GM_info.script.version !== GM_getValue('previousVersion', '0.0.0')) {
-    GM_setValue('previousVersion', GM_info.script.version)
-    const changelog = `
-    <div style="margin: 5px 0px">
-      <b>版本：</b>${GM_info.script.version}
-    </div>
-    <div style="margin: 5px 0px">
-      <b>HoloDex：</b>
-      <ul style="margin: 2px 0px;padding-left: 30px;">
-        <li>支援HoloDex。</li>
-      </ul>
-    </div>
-    <div style="margin: 5px 0px">
-      <b>HoloTools：</b>
-      <ul style="margin: 2px 0px;padding-left: 30px;">
-        <li>新增按鈕在右上方控制列中，可以關閉聊天室讓出空間(限舊版)。</li>
-      </ul>
-    </div>
-    `
+  const nowVerion = GM_info.script.version
+  if (nowVerion !== GM_getValue('previousVersion', '0.0.0')) {
+    GM_setValue('previousVersion', nowVerion)
+    const msg = {
+      版本: [
+        '新增黑名單功能。',
+        '新增標題搜尋功能。'
+      ],
+      HoloDex: [
+        '支援HoloDex。'
+      ],
+      HoloTools: [
+        '新增按鈕在右上方控制列中，可以關閉聊天室讓出空間(限舊版)。'
+      ],
+      Twitch: [],
+      Nijimado: [],
+      Youtube: []
+    }
+    let changelog = ''
+    for (const key in msg) {
+      if (msg[key].length !== 0) {
+        let tmp = ''
+        for (let index = 0; index < msg[key].length; index++) {
+          tmp = String.prototype.concat(tmp, `<li>${msg[key][index]}</li>`)
+        }
+        changelog = String.prototype.concat(changelog, `<div style="margin: 5px 0px"><b>${key}：</b>`)
+        if (key === '版本') changelog = String.prototype.concat(changelog, `${nowVerion}`)
+        changelog = String.prototype.concat(changelog, '<ul style="margin: 2px 0px;padding-left: 30px;">', tmp, '</ul></div>')
+      }
+    }
     const modal = $(`
     <div id="PTTChangeLog" class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-hidden="true" style="color: #000">
       <div class="modal-dialog modal-dialog-centered">
