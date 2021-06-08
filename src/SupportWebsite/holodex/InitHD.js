32c3d2c0fac3e8de04955234b40def398deaed3c
crimsonmoon9 <crimsonmoon9@users.noreply.github.com>
2021-06-08 17:12:01 +0800

    修改按鈕顏色、collapse動畫，在每個空cell中加上PTT啟動按鈕。

--- src/SupportWebsite/holodex/InitHD.js
+++ src/SupportWebsite/holodex/InitHD.js
@@ -9,21 +9,33 @@ export function InitHD (messageposter) {
 
   // run app instance loop
   const watchcheck = /https:\/\/holodex\.net\/multiview/.exec(window.location.href)
+  let pttBootBtnCount = 0; let count = 0
   if (watchcheck) {
     setInterval(() => {
       const btnParentSet = $('.px-0.d-flex.flex-grow-1.align-stretch.mb-1')
-      if ($('#pttBootBtn').length === 0 && btnParentSet.length > 0 && $('#PTTChat').length === 0) {
-        const btnParent = btnParentSet.eq(0)
-        btnParent.css('flex-direction', 'column')
-        btnParent.children().eq(1).clone().attr('id', 'pttBootBtn').appendTo(btnParent).attr('style', 'background-color: crimson !important').css({ 'margin-top': '15px', 'flex-basis': '100%', padding: '6px 0px', 'max-width': '608px' })
-        btnParent.children().eq(2).find('path').attr('d', 'M13 3H6v18h4v-6h3c3.31 0 6-2.69 6-6s-2.69-6-6-6zm.2 8H10V7h3.2c1.1 0 2 .9 2 2s-.9 2-2 2z')
-        btnParent.prepend($('<div class="d-flex"></div>').prepend(btnParent.children().eq(0), btnParent.children().eq(1)))
-        const gridIndex = btnParent.parents().eq(3).index()
-        $('#pttBootBtn').on('click', function () {
-          btnParent.children().eq(0).children().eq(1).trigger('click')
-          installPTT(gridIndex)
+      if (btnParentSet.length > pttBootBtnCount) {
+        btnParentSet.each(function (index) {
+          const btnParent = btnParentSet.eq(index)
+          if (!(btnParent.children().eq(0).hasClass('d-flex'))) {
+            btnParent.css('flex-direction', 'column')
+            btnParent.children().eq(1).clone().attr({ id: 'pttBootBtn', ins: `${count}` }).appendTo(btnParent).attr('style', 'background-color: rgb(150, 0, 180) !important').css({ 'margin-top': '15px', 'flex-basis': '100%', padding: '6px 0px', 'max-width': '608px' })
+            btnParent.children().eq(2).find('path').attr('d', 'M13 3H6v18h4v-6h3c3.31 0 6-2.69 6-6s-2.69-6-6-6zm.2 8H10V7h3.2c1.1 0 2 .9 2 2s-.9 2-2 2z')
+            btnParent.prepend($('<div class="d-flex"></div>').prepend(btnParent.children().eq(0), btnParent.children().eq(1)))
+            console.log(count)
+            const currentBtn = $(`#pttBootBtn[ins="${count}"]`)
+            currentBtn.on('click', function () {
+              console.log('click')
+              if ($('#PTTChat').length === 0) {
+                const gridIndex = currentBtn.parents().eq(4).index()
+                btnParent.children().eq(0).children().eq(1).trigger('click')
+                installPTT(gridIndex)
+              }
+            })
+            count++
+          }
         })
       }
+      pttBootBtnCount = btnParentSet.length
     }, 1000)
   }
   function installPTT (gridIndex) {
@@ -36,26 +48,25 @@ export function InitHD (messageposter) {
           ChangeLog()
           const chatBtn = gridParent.find($('span:contains("Chat")')).parent()
           $('#PTTChat').addClass('h-100').css('background', 'transparent')
-          $('#PTTMain').addClass('h-100').removeClass('position-absolute')
-          $('#PTTChat-app').addClass('h-100')
+          $('#PTTMain').removeClass('position-absolute')
           $('#PTTChat-contents').css('height', '')
           if (chatBtn.css('background-color') !== 'rgb(39, 39, 39)') {
             chatBtn.trigger('click')
           }
           $('#PTTMainBtn').on('click', function () {
             const originChat = gridParent.children().children().eq(0).children().eq(1)
-            // const h = $('#PTTChat').height()
+            const h = $('#PTTChat').parent().height()
             if (originChat.css('z-index') === '-1') {
-              // $('#PTTMain').removeClass('h-100').css('max-height', h + 'px')
-              // $('#PTTChat-app').removeClass('h-100').css('max-height', h + 'px')
+              $('#PTTMain').removeClass('h-100').css('max-height', h + 'px')
+              $('#PTTChat-app').removeClass('h-100').css('max-height', h + 'px')
               originChat.css('z-index', '0')
             } else {
-              // $('#PTTMain').css('max-height', h + 'px')
-              // $('#PTTChat-app').css('max-height', h + 'px')
-              // $('#PTTMain').on('shown.bs.collapse', function () {
-              //   $('#PTTMain').addClass('h-100')
-              //   $('#PTTChat-app').addClass('h-100')
-              // })
+              $('#PTTMain').css('max-height', h + 'px')
+              $('#PTTChat-app').css('max-height', h + 'px')
+              $('#PTTMain').on('shown.bs.collapse', function () {
+                $('#PTTMain').addClass('h-100').css('max-height', '')
+                $('#PTTChat-app').addClass('h-100').css('max-height', '')
+              })
               originChat.css('z-index', '-1')
             }
           })
@@ -86,7 +97,7 @@ export function InitHD (messageposter) {
     const t = setInterval(() => {
       const btnParent = gridParent.find($('.cell-control')).children()
       if (btnParent) {
-        btnParent.children().eq(0).attr({ disabled: 'true', title: '鎖定：PTT運行中' })
+        btnParent.children().eq(0).attr({ disabled: 'true', title: '鎖定：PTT運行中', style: 'background-color: rgb(150, 150, 150) !important' })
         btnParent.children().eq(1).on('click', function () {
           gridParent.children().children().eq(0).children().eq(0).css({ height: '100%', width: '100%' })
           listenEditBtn(gridParent)
